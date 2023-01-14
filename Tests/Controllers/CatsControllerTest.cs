0c003081297a46d82baa10390e70b719c481b9c7
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-01-14 11:20:52 +0100

    :construction: WIP on #39

---
+++ Tests/Controllers/CatsControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cat_cafe.Controllers;
+using cat_cafe.Dto;
+using cat_cafe.Mappers;
+using cat_cafe.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat_cafe.Controllers.Tests
+{
+    [TestClass()]
+    public class CatsControllerTest
+    {
+        [TestMethod()]
+        public void GetCatsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCatTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutCatTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostCatTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteCatTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CatExistsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
