d663fe8b38a4ab6c46f59252923fda0c26b62b86
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-01-14 09:22:54 +0100

    :sparkles: Implement DTO + mapper profile in controller

--- cat_cafe/Mappers/CatMapper.cs
+++ cat_cafe/Mappers/CatMapper.cs
@@ -1,7 +1,14 @@
-﻿namespace cat_cafe.Mappers
-{
-    public class CatMapper
-    {
+﻿using AutoMapper;
+using cat_cafe.Dto;
+using cat_cafe.Entities;
 
+namespace cat_cafe.Mappers
+{
+    public class CatMapper : Profile
+    {
+        public CatMapper()
+        {
+            CreateMap<Cat, CatDto>().ReverseMap();
+        }
     }
 }
