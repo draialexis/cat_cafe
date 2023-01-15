de7cec4d082a86cc0bb498d59733a16f28a77c83
Najlae Lambaraa <Najlae.LAMBARAA@etu.uca.fr>
2023-01-15 14:29:43 +0100

    :seedling: Add BarDto

---
+++ cat_cafe/Dto/BarDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cat_cafe.Dto
+{
+    public class BarDto
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public List<Cat> cats { get; set; } = new List<Cat>();
+    }
+}
---
+++ cat_cafe/Mappers/BarMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cat_cafe.Dto;
+using cat_cafe.Entities;
+namespace cat_cafe.Mappers
+{
+    public class BarMapper : Profile
+    {
+        public BarMapper(){
+
+            CreateMap<Bar,BarMapper>().ReverseMap();
+        }
+       
+    }
+}
