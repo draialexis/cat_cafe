8645b282f90765ba52f83c1176c835f4b978db6a
ismail.taha_janan <ismail.taha_janan@etu.uca.fr>
2023-01-18 16:23:01 +0100

    age and name required for customer

--- cat_cafe/Dto/CustomerDto.cs
+++ cat_cafe/Dto/CustomerDto.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace cat_cafe.Dto
 {
 	public class CustomerDto
 	{
         public long Id { get; set; }
-        public string? FullName { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
         public int Age { get; set; } = 0;
     }
 }
